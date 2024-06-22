e8c258b4daaba7d92bcaf3c56d2b570fd03996fa
Gianpaolo Macario <gianpaolo.macario@arol.com>
2024-06-22 13:18:31 +0000

    src/pages/index.js: Comment out lines to remove build warnings

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,28 @@
 import * as React from 'react';
 
 import AttributeGrid from '../components/AttributeGrid';
-import Container from '../components/Container';
+// import Container from '../components/Container';
 import Hero from '../components/Hero';
-import BlogPreviewGrid from '../components/BlogPreviewGrid';
-import Highlight from '../components/Highlight';
+// import BlogPreviewGrid from '../components/BlogPreviewGrid';
+// import Highlight from '../components/Highlight';
 import Layout from '../components/Layout/Layout';
-import ProductCollectionGrid from '../components/ProductCollectionGrid';
-import ProductCardGrid from '../components/ProductCardGrid';
+// import ProductCollectionGrid from '../components/ProductCollectionGrid';
+// import ProductCardGrid from '../components/ProductCardGrid';
 import Quote from '../components/Quote';
-import Title from '../components/Title';
+// import Title from '../components/Title';
 
-import { generateMockBlogData, generateMockProductData } from '../helpers/mock';
+// import { generateMockBlogData, generateMockProductData } from '../helpers/mock';
 
 import * as styles from './index.module.css';
-import { Link, navigate } from 'gatsby';
+// import { Link, navigate } from 'gatsby';
 
 const IndexPage = () => {
-  const newArrivals = generateMockProductData(3, 'shirt');
-  const blogData = generateMockBlogData(3);
+  // const newArrivals = generateMockProductData(3, 'shirt');
+  // const blogData = generateMockBlogData(3);
 
-  const goToShop = () => {
-    navigate('/shop');
-  };
+  // const goToShop = () => {
+  //   navigate('/shop');
+  // };
 
   return (
     <Layout disablePaddingBottom>
