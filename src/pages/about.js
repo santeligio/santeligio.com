b2ce78febc7801d63b9b336d9de2861b5ae53520
Gianpaolo Macario <gianpaolo.macario@arol.com>
2024-07-07 06:16:46 +0000

    Aggiunto estratto della Convenzione del 1858

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,23 +2,23 @@ import React, { useRef } from 'react';
 
 import Container from '../components/Container';
 import Hero from '../components/Hero';
-// import ThemeLink from '../components/ThemeLink';
+import ThemeLink from '../components/ThemeLink';
 import Layout from '../components/Layout/Layout';
 
 import * as styles from './about.module.css';
 const AboutPage = (props) => {
   let historyRef = useRef();
-  // let valuesRef = useRef();
+  let sociRef = useRef();
   // let sustainabilityRef = useRef();
 
-  // const handleScroll = (elementReference) => {
-  //   if (elementReference) {
-  //     window.scrollTo({
-  //       behavior: 'smooth',
-  //       top: elementReference.current.offsetTop - 280,
-  //     });
-  //   }
-  // };
+  const handleScroll = (elementReference) => {
+    if (elementReference) {
+      window.scrollTo({
+        behavior: 'smooth',
+        top: elementReference.current.offsetTop - 280,
+      });
+    }
+  };
 
   return (
     <Layout disablePaddingBottom>
@@ -26,50 +26,55 @@ const AboutPage = (props) => {
         {/* Hero Container */}
         <Hero
           maxWidth={'900px'}
-          image={'/santeligio_2023.jpg'}
+          image={'/santeligio/20190818-convenzione-1858-pag1.jpg'}
           title={`Compagnia di Sant'Eligio \n Chi siamo`}
         />
 
-        {/* <div className={styles.navContainer}>
+        <div className={styles.navContainer}>
           <ThemeLink onClick={() => handleScroll(historyRef)} to={'#history'}>
-            History
+            Storia
           </ThemeLink>
-          <ThemeLink onClick={() => handleScroll(valuesRef)} to={'#values'}>
-            Values
+          <ThemeLink onClick={() => handleScroll(sociRef)} to={'#values'}>
+            Elenco Soci
           </ThemeLink>
-          <ThemeLink
+          {/* <ThemeLink
             onClick={() => handleScroll(sustainabilityRef)}
             to={'#sustainability'}
           >
             Sustainability
-          </ThemeLink>
-        </div> */}
+          </ThemeLink> */}
+        </div>
 
         <Container size={'large'} spacing={'min'}>
           <div className={styles.detailContainer} ref={historyRef}>
             <p>
-              Fondata nel 1858 da una ventina di Soci che praticavano la
-              professione del Carrettiere (in dialetto Vernantese, "Cartouné").
-              La professione del "Cartouné" - che ai giorni nostri potremmo
-              paragonare a quella del camionista, rappresentava un mestiere
-              molto redditizio nella Valle Vermenagna, da sempre via di
-              collegamento tra la Provincia di Cuneo e il mare. La Compagnia di
-              Sant'Eligio e' costituita dai discendenti dei Soci Fondatori e si
-              tramanda gelosamente per linea gerarchica al discendente maschio
+              La più antica testimonianza della Compagnia di Sant'Eligio di Vernante
+              risale al 5 aprile 1858, data della Convenzione sottoscritta
+              da una ventina di Soci che praticavano la professione del Carrettiere
+              (in dialetto Vernantese, "Cartouné").
+            </p>
+            <p>  
+              La professione del "Cartouné" - che potremmo paragonare a quella
+              del camionista dei giorni nostri, rappresentava un mestiere molto
+              redditizio nella Valle Vermenagna, da sempre via di collegamento
+              tra la Provincia di Cuneo e il mare.
+            </p>
+            <p>
+              La Compagnia di Sant'Eligio e' costituita dai discendenti dei Soci Fondatori
+              e si tramanda rigorosamente per linea gerarchica al discendente maschio
               più giovane.
             </p>
           </div>
         </Container>
 
-        {/* <div className={styles.imageContainer}>
-          <img alt={'shirt brand'} src={'/about1.png'}></img>
-        </div> */}
+        <div className={styles.imageContainer}>
+          <img alt={'Convenzione 1858'} src={'/santeligio_2023.jpg'}></img>
+        </div>
 
         <Container size={'large'} spacing={'min'}>
           <div className={styles.content}>
             {<h3>I Soci della Compagnia</h3>}
-            {<div>
-              {/* ref={valuesRef} */}
+            {<div ref={sociRef}>
               <p>
                 (Ultimo aggiornamento: 6 luglio 2024)
               </p>
